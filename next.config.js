8c913baf14af73a7776ba9c9236a471d9b244c92
brusooo <jenifar200227@gmail.com>
2022-08-07 21:34:57 +0530

    sample

--- next.config.js
+++ next.config.js
@@ -2,9 +2,15 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    ORIGIN: 'https://count-on-you.netlify.app',
-    NEXTAUTH_SECRET : 'f20f360415671afb7200f42f79f6e373'
+    ORIGIN: "http://localhost:3000",
+    NEXTAUTH_SECRET: "f20f360415671afb7200f42f79f6e373",
+    NEXTAUTH_URL: "https://count-on-you.netlify.app",
+    GOOGLE_ID:
+      "566841125763-debuci6rs2fnjeau9upfskh1m2aobvtu.apps.googleusercontent.com",
+    GOOGLE_SECRET: "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",
+    MONGODB_URI:
+      "mongodb+srv://brusooo:Jeni2002@cluster0.bcpu8.mongodb.net/?retryWrites=true&w=majority",
   },
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
