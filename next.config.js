06ab79bc7245bae7d12bfc804f068fc308e1f4ae
brusooo <jenifar200227@gmail.com>
2022-08-07 21:40:15 +0530

    count

--- next.config.js
+++ next.config.js
@@ -2,15 +2,10 @@
 const nextConfig = {
   reactStrictMode: false,
   env: {
-    ORIGIN: "http://localhost:3000",
-    NEXTAUTH_SECRET: "f20f360415671afb7200f42f79f6e373",
-    NEXTAUTH_URL: "https://count-on-you.netlify.app",
-    GOOGLE_ID:
-      "566841125763-debuci6rs2fnjeau9upfskh1m2aobvtu.apps.googleusercontent.com",
-    GOOGLE_SECRET: "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",
-    MONGODB_URI:
-      "mongodb+srv://brusooo:Jeni2002@cluster0.bcpu8.mongodb.net/?retryWrites=true&w=majority",
+    ORIGIN: 'https://count-on-you.netlify.app',
+    NEXTAUTH_SECRET : 'f20f360415671afb7200f42f79f6e373',
+    NEXTAUTH_URL :'https://count-on-you.netlify.app'
   },
-};
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
