d135c2482368dcd259baced135fb25300e6f0e87
brusooo <jenifar200227@gmail.com>
2022-12-11 00:00:49 +0530

    ....

--- pages/api/auth/emaillogin.js
+++ pages/api/auth/emaillogin.js
@@ -4,37 +4,42 @@ import bcrypt from 'bcryptjs';
 
 
 export default async function handler(req, res) {
-  const client = await clientPromise;
-  const db = client.db("MyUsers");
-  const todoUsers = db.collection("users");
-  switch (req.method) {
-    case "POST":
-      let bodyObject = req.body;
-      const user = await todoUsers
-      .find({ name : bodyObject.name }) 
-      .toArray();
-      if (user.length == 0) {
-        let newUser = await todoUsers.insertOne(bodyObject);
-        return res.json({ result: "Successful" });
-      }
-      return res.json({ result : "Unsuccessful" });
+  try {
+    const client = await clientPromise;
+    const db = client.db("MyUsers");
+    const todoUsers = db.collection("users");
 
-    case "GET":
-      const users = await todoUsers
-        .find({
-          name: req.query.name,
-          email: req.query.email,
-        })
-        .toArray();
-      
-      
-      if(bcrypt.compareSync(req.query.password, users[0].password))
-      { 
-        res.json(users[0]); }
-      else{
-        return false
-      }
+    switch (req.method) {
+      case "POST":
+        let bodyObject = req.body;
+        const user = await todoUsers
+          .find({ name: bodyObject.name })
+          .toArray();
+        if (user.length == 0) {
+          let newUser = await todoUsers.insertOne(bodyObject);
+          return res.json({ result: "Successful" });
+        }
+        return res.json({ result: "Unsuccessful" });
 
-      break;
+      case "GET":
+        const users = await todoUsers
+          .find({
+            name: req.query.name,
+            email: req.query.email,
+          })
+          .toArray();
+
+
+        if (bcrypt.compareSync(req.query.password, users[0].password)) {
+          res.json(users[0]);
+        }
+        else {
+          return false
+        }
+
+        break;
+    }l̥
+  } catch (error) {
+    return res.json({ result: "Unsuccessful" });
   }
 }
