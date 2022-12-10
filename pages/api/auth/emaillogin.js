ceed8a5afe8c4c809578f2a3ca7e3fffde455c9d
brusooo <jenifar200227@gmail.com>
2022-12-11 00:11:39 +0530

    ...

--- pages/api/auth/emaillogin.js
+++ pages/api/auth/emaillogin.js
@@ -4,42 +4,44 @@ import bcrypt from 'bcryptjs';
 
 
 export default async function handler(req, res) {
+
+
   try {
     const client = await clientPromise;
     const db = client.db("MyUsers");
     const todoUsers = db.collection("users");
-
-    switch (req.method) {
-      case "POST":
-        let bodyObject = req.body;
-        const user = await todoUsers
-          .find({ name: bodyObject.name })
-          .toArray();
-        if (user.length == 0) {
-          let newUser = await todoUsers.insertOne(bodyObject);
-          return res.json({ result: "Successful" });
-        }
-        return res.json({ result: "Unsuccessful" });
-
-      case "GET":
-        const users = await todoUsers
-          .find({
-            name: req.query.name,
-            email: req.query.email,
-          })
-          .toArray();
-
-
-        if (bcrypt.compareSync(req.query.password, users[0].password)) {
-          res.json(users[0]);
-        }
-        else {
-          return false
-        }
-
-        break;
-    }l̥
   } catch (error) {
-    return res.json({ result: "Unsuccessful" });
+    return res.json({ result: "error" });
+  }
+
+  switch (req.method) {
+    case "POST":
+      let bodyObject = req.body;
+      const user = await todoUsers
+        .find({ name: bodyObject.name })
+        .toArray();
+      if (user.length == 0) {
+        let newUser = await todoUsers.insertOne(bodyObject);
+        return res.json({ result: "Successful" });
+      }
+      return res.json({ result: "Unsuccessful" });
+
+    case "GET":
+      const users = await todoUsers
+        .find({
+          name: req.query.name,
+          email: req.query.email,
+        })
+        .toArray();
+
+
+      if (bcrypt.compareSync(req.query.password, users[0].password)) {
+        res.json(users[0]);
+      }
+      else {
+        return false
+      }
+
+      break;
   }
 }
