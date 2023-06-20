14b9df4724282fc434241713986aea5ffe7183c9
Jenifar <76559684+brusooo@users.noreply.github.com>
2023-06-20 18:52:58 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
-  env: {
-    ORIGIN: 'https://countonyou.netlify.app',
-    NEXTAUTH_SECRET : 'f20f360415671afb7200f42f79f6e373',
-    NEXTAUTH_URL : 'https://countonyou.netlify.app',
-    SECRET_KEY : 'f20f360415671afb7200f42f7'
-  },
+  
 }
 
 module.exports = nextConfig
